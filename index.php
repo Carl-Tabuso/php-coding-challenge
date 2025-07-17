2ef72705fe6dde857e0d6d7b859bc6869bfe5f9d
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-07-17 10:04:38 +0800

    add comments for each step

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+// file for
+// reading: logs.txt
+// writing: output.txt
+
+// 1st step
+// stream the reading file, 10KB at a time
+// format each row into <UserID>|<BytesTX|<BytesRX|<DateTime>|<ID>
+// append to writing file
+// store in array for 2nd step
+// repeat
+
+// add a breakline to the file
+
+// 2nd step
+// perform bubble sort of the IDs and store in array
+// loop through the sorted array and append to the writing file
+
+// add a breakline to the file
+
+// 3rd step
+// perform bubble sort of the UserIDs and store in array
+// loop through the sorted array and append to the writing file
+
+$path = __DIR__.'/logs.txt';
+
+if (! file_exists($path)) die('File not found!');
+
+$file = fopen($path, 'rb');
+var_dump($file);
+
+while (! feof($file)) {
+    var_dump(fread($file, 10240));
+}
